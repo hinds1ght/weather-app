e104bf60bd0a48030bfbc432bb032c124ea2d4c7
hinds1ght <markgil_yu@yahoo.com>
2025-07-02 14:01:45 +0800

    just creating repo this old simple app i did

---
+++ script.js
@@ -0,0 +1,32 @@
+let form = document.querySelector('form');
+let input = document.getElementById('city');
+let country = document.querySelector('.country');
+let region = document.querySelector('.region');
+let stat = document.querySelector('.status');
+let icon = document.querySelector('img');
+let appkey = 'be88403500854de39ba44427230910';
+let http = 'https:'
+
+async function takeData(theCity){
+    let weather = await fetch(`https://api.weatherapi.com/v1/current.json?key=${appkey}&q=${theCity}`, { mode: 'cors'})
+    let theobj = weather.json();
+    return theobj;
+}
+
+form.addEventListener('submit', (e) =>{
+    e.preventDefault();
+    
+    takeData(input.value.trim())
+        .then( r =>{
+            display(r);
+        });
+
+})
+
+function display(res) {
+    console.log(res)
+    country.textContent = `Country: ${res.location.country}`
+    region.textContent = `Region: ${res.location.region}`
+    stat.textContent = `Weather: ${res.current.condition.text}`
+    icon.src = http.concat(res.current.condition.icon);
+}
